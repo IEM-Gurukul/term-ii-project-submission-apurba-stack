4f555be3c21df1affff417af0b1f6720472e3f00
apurba-stack <apurbadyctc1@gmail.com>
2026-03-26 23:51:23 +0530

    Create GUIMain.java

---
+++ src/main/GUIMain.java
@@ -0,0 +1,13 @@
+package main;
+
+import gui.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class GUIMain {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
